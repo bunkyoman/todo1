52e89f0ee4b71e0009008b387284ed460a6309be
bunkyoman <qnszy1347@gmail.com>
2023-11-17 10:42:34 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
+export default {
+  components: {
+    "TodoHeader": TodoHeader,
+    "TodoInput": TodoInput,
+    "TodoList": TodoList,
+    "TodoFooter": TodoFooter,
+}
+}
+</script>
+
+<style scoped>
+
+h1 {
+  color: #2F3B52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
+
+</style>
